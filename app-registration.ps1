2dec09060961e5ee2d6cfa60188cf779e3dff43e
gandresto-tecbot <80547027+gandresto-tecbot@users.noreply.github.com>
2021-04-07 19:02:04 -0500

    Agrego script de app registration

---
+++ app-registration.ps1
@@ -0,0 +1,2 @@
+az account set --subscription "e209c568-0f60-4c6d-ae25-7f3a24b518db"
+az ad app create --display-name "chbot-inte2-pprd" --password "~iInM7-5S~t~RKo29Yz6d~i6fyOq3z3PEX" --available-to-other-tenants
